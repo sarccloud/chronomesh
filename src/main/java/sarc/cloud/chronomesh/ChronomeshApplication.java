342040c8ff2394bf6ee70f0a16d06e75d4782eef
Marco Aurelio Souza Mangan <10067320@pucrs.br>
2024-11-12 18:50:20 -0300

    #2

---
+++ src/main/java/sarc/cloud/chronomesh/ChronomeshApplication.java
@@ -0,0 +1,13 @@
+package sarc.cloud.chronomesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronomeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronomeshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sarc/cloud/chronomesh/ChronomeshApplicationTests.java
@@ -0,0 +1,13 @@
+package sarc.cloud.chronomesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronomeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
